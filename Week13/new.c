7183738328f9d06045af29c9931e66541c9c38a8
Itay <itaynewman@gmial.com>
2024-02-05 18:44:27 +0200

    fix

---
+++ Week13/new.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+#define COLS 8
+#define ROWS 8
+
+int main(void)
+{
+    char checkers[8][8] = {'b'};
+    int i = 0;
+    int j = 0;
+    for(i = 0; i < COLS; i++)
+    {
+        for(j = 0; j < ROWS; j++)
+        {
+            if((i + j) % 2 == 0)
+            {
+                checkers[i][j] = 'b';
+            }
+            else
+            {
+                checkers[i][j] = 'w';
+            }
+        }
+    }
+    for(i = 0; i < COLS; i++)
+    {
+        for(j = 0; j < ROWS; j++)
+        {
+            printf("%c ", checkers[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
